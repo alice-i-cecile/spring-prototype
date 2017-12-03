811b827ae4a3472340465aed77731c8e4483bb83
Alice Cecile <alice.i.cecile@gmail.com>
2017-12-03 11:21:44 -0500

    Cleaned up out-of-bounds behaviour

--- site/javascripts/util.js
+++ site/javascripts/util.js
@@ -1,20 +1,3 @@
-/**
- * Returns a number whose value is limited to the given range.
- *
- * Example: limit the output of this computation to between 0 and 255
- * <pre>
- * (x * 255).clamp(0, 255)
- * </pre>
- *
- * @param {Number} min The lower boundary of the output range
- * @param {Number} max The upper boundary of the output range
- * @returns A number in the range [min, max]
- * @type Number
- */
-Number.prototype.clamp = function(min, max) {
-  return Math.min(Math.max(this, min), max);
-};
-
 // n-dimensional array creation
 // From: https://stackoverflow.com/questions/966225/how-can-i-create-a-two-dimensional-array-in-javascript/966938#966938
 function createArray(length) {

650c217ad7f34982c18d202cf496f152d6ae540e
Alice Cecile <alice.i.cecile@gmail.com>
2017-12-02 20:04:58 -0500

    Added springs w/ visuals

--- site/javascripts/util.js
+++ site/javascripts/util.js
@@ -14,3 +14,17 @@
 Number.prototype.clamp = function(min, max) {
   return Math.min(Math.max(this, min), max);
 };
+
+// n-dimensional array creation
+// From: https://stackoverflow.com/questions/966225/how-can-i-create-a-two-dimensional-array-in-javascript/966938#966938
+function createArray(length) {
+    var arr = new Array(length || 0),
+        i = length;
+
+    if (arguments.length > 1) {
+        var args = Array.prototype.slice.call(arguments, 1);
+        while(i--) arr[length-1 - i] = createArray.apply(this, args);
+    }
+
+    return arr;
+}
